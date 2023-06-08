9d2bb6833cb3ea5b620aecaef602099eda9d2ae8
sbaldu <simone.balducci00@gmail.com>
2023-06-08 15:16:01 +0200

    Write gradient methods for activators
    Fixes in Activators header
    
    Fixes in Activators header

--- src/serial/include/Activators.hpp
+++ src/serial/include/Activators.hpp
@@ -6,7 +6,15 @@
 
 #include <algorithm>
 #include <cmath>
+#include <memory>
+#include <vector>
 
+#include "Layer.hpp"
+
+template <typename T>
+using shared = std::shared_ptr<T>;
+
+template <typename T>
 struct Step {
   short operator()(double x) {
     if (x < 0) {
@@ -15,20 +23,88 @@ struct Step {
       return 1;
     }
   }
+
+  // Derivative of the activation function
+  double grad(double x) { return 0; }
+  std::vector<double> grad(shared<Layer<T>> layer) { 
+	return std::vector<double>(layer->size(), 0);
+  }
+  std::vector<double> grad(std::vector<T> node_values) {
+	return std::vector<double>(node_values.size(), 0);
+  }
 };
 
+template <typename T>
 struct Linear {
   double operator()(double x) { return x; }
+
+  // Derivative of the activation function
+  double grad(double x) { return 1; }
+  std::vector<double> grad(shared<Layer<T>> layer) {
+	return std::vector<double>(layer->size(), 1);
+  }
+  std::vector<double> grad(std::vector<T> node_values) {
+	return std::vector<double>(node_values.size(), 1);
+  }
 };
 
+template <typename T>
 struct Sigmoid {
   double operator()(double x) { return 1. / (1 + std::exp(-x)); }
+
+  // Derivative of the activation function
+  double grad(double x) {
+	return Sigmoid()(x) * (1 - Sigmoid()(x));
+  }
+  std::vector<double> grad(shared<Layer<T>> layer) {
+	int N{layer->size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(layer->nodes()[i]);
+	}
+
+	return gradient_values;
+  }
+  std::vector<double> grad(std::vector<T> node_values) {
+	int N{node_values.size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(node_values[i]);
+	}
+
+	return gradient_values;
+  }
 };
 
+template <typename T>
 struct Tanh {
   double operator()(double x) { return std::tanh(x); }
+
+  // Derivative of the activation function
+  double grad(double x) {
+	return 1 + pow(std::tanh(x), 2);
+  }
+  std::vector<double> grad(shared<Layer<T>> layer) {
+	int N{layer->size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(layer->nodes()[i]);
+	}
+
+	return gradient_values;
+  }
+  std::vector<double> grad(std::vector<T> node_values) {
+	int N{node_values.size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(node_values[i]);
+	}
+
+	return gradient_values;
+  }
 };
 
+template <typename T>
 struct Elu {
   double A;
 
@@ -39,22 +115,128 @@ struct Elu {
       return A * (std::exp(x) - 1);
     }
   }
+
+  // Derivative of the activation function
+  double grad(double x) {
+	if (x >= 0) {
+	  return 1;
+	} else {
+	  return A * std::exp(x);
+	}
+  }
+  std::vector<double> grad(shared<Layer<T>> layer) {
+	int N{layer->size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(layer->nodes()[i]);
+	}
+
+	return gradient_values;
+  }
+  std::vector<double> grad(std::vector<T> node_values) {
+	int N{node_values.size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(node_values[i]);
+	}
+
+	return gradient_values;
+  }
 };
 
-struct Leaky_Elu {
+template <typename T>
+struct Leaky_ReLU {
   double operator()(double x) { return std::max(0.1 * x, x); }
+
+  // Derivative of the activation function
+  double grad(double x) {
+	if (x >= 0) {
+	  return 1;
+	} else {
+	  return 0.1;
+	}
+  }
+  std::vector<double> grad(shared<Layer<T>> layer) {
+	int N{layer->size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(layer->nodes()[i]);
+	}
+
+	return gradient_values;
+  }
+  std::vector<double> grad(std::vector<T> node_values) {
+	int N{node_values.size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(node_values[i]);
+	}
+
+	return gradient_values;
+  }
 };
 
-struct Parametric_Elu {
+template <typename T>
+struct Parametric_ReLU {
   double A;
 
   double operator()(double x) { return std::max(A * x, x); }
+
+  // Derivative of the activation function
+  double grad(double x) {
+	if (x >= 0) {
+	  return 1;
+	} else {
+	  return A;
+	}
+  }
+  std::vector<double> grad(shared<Layer<T>> layer) {
+	int N{layer->size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(layer->nodes()[i]);
+	}
+
+	return gradient_values;
+  }
+  std::vector<double> grad(std::vector<T> node_values) {
+	int N{node_values.size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(node_values[i]);
+	}
+
+	return gradient_values;
+  }
 };
 
+template <typename T>
 struct Swish {
   double operator()(double x) {
-    Sigmoid sig;
-    return x * sig(x);
+    return x * Sigmoid<T>()(x);
+  }
+
+  // Derivative of the activation function
+  double grad(double x) {
+	return Sigmoid<T>()(x) * (1 + x) * (1 - Sigmoid<T>()(x));
+  }
+  std::vector<double> grad(shared<Layer<T>> layer) {
+	int N{layer->size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(layer->nodes()[i]);
+	}
+
+	return gradient_values;
+  }
+  std::vector<double> grad(std::vector<T> node_values) {
+	int N{node_values.size()};
+	std::vector<double> gradient_values(N);
+	for (int i{}; i < N; ++i) {
+	  gradient_values[i] = grad(node_values[i]);
+	}
+
+	return gradient_values;
   }
 };
 
