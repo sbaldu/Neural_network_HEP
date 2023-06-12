40497b08f47630b716b8d4d3000f48d6c573c22a
sbaldu <simone.balducci00@gmail.com>
2023-06-12 16:35:36 +0200

    Add test of neural network for Or and And functions

---
+++ test/NetworkTest/OrFunction.cc
@@ -0,0 +1,22 @@
+
+#include <iostream>
+
+#include "../../src/serial/include/Network.hpp"
+#include "../../src/serial/include/ErrorFunction.hpp"
+
+int main() {
+  double eta{0.1};
+  Network<int, double, Sigmoid, MeanSquaredError> net(2, {2, 1});
+  std::vector<std::vector<int>> inputs{{0, 0}, {0, 1}, {1, 0}, {1, 1}};
+  std::vector<std::vector<int>> targets{{0}, {1}, {1}, {1}};
+
+  int n_epochs{100};
+  for (int epoch{}; epoch < n_epochs; ++epoch) {
+	for (int i{}; i < inputs.size(); ++i) {
+	  net.load_input_layer(inputs[i]);
+	  net.train(inputs[i], targets[i], eta);
+	  std::cout << net.output_layer()[0] << std::endl;
+	}
+	std::cout << "-------------------------------------\n";
+  }
+}
