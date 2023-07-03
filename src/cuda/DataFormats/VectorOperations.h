7cc49f1a9bea730dbf7857783e040d9be324d1a3
sbaldu <simone.balducci00@gmail.com>
2023-07-03 09:02:37 +0200

    Write cuda version of vector operations header

---
+++ src/cuda/DataFormats/VectorOperations.h
@@ -0,0 +1,171 @@
+
+#ifndef vec_operations_h
+#define vec_operations_h
+
+#pragma once
+
+#include <algorithm>
+#include <concepts>
+#include <ostream>
+#include <vector>
+
+#include "VectorKernels.h"
+#include "Matrix.h"
+
+// Overload sum operator for vectors
+template <typename T>
+std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2) {
+  int N{v1.size()};
+  std::vector<T> result(N);
+
+  int size{sizeof(T) * N};
+
+  // Allocate on device
+  T *d_v1, *d_v2, *d_res;
+  cudaMalloc(&d_v1, size);
+  cudaMalloc(&d_v2, size);
+  cudaMalloc(&d_res, size);
+
+  // Launch kernel
+  cudaMemcpy(d_v1, v1.data(), size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_v2, v2.data(), size, cudaMemcpyHostToDevice);
+  vec_add<<<N/256, 256>>>(d_v1, d_v2, d_res, N);
+  cudaMemcpy(result.data(), d_res, size, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_v1);
+  cudaFree(d_v2);
+  cudaFree(d_res);
+
+  return result;
+}
+
+// Overload subtraction
+template <typename T>
+std::vector<T> operator-(const std::vector<T>& v1, const std::vector<T>& v2) {
+  int N{v1.size()};
+  std::vector<T> result(N);
+
+  int size{sizeof(T) * N};
+
+  // Allocate on device
+  T *d_v1, *d_v2, *d_res;
+  cudaMalloc(&d_v1, size);
+  cudaMalloc(&d_v2, size);
+  cudaMalloc(&d_res, size);
+
+  // Launch kernel
+  cudaMemcpy(d_v1, v1.data(), size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_v2, v2.data(), size, cudaMemcpyHostToDevice);
+  vec_sub<<<N/256, 256>>>(d_v1, d_v2, d_res, N);
+  cudaMemcpy(result.data(), d_res, size, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_v1);
+  cudaFree(d_v2);
+  cudaFree(d_res);
+
+  return result;
+}
+
+// Overload product of vector with constant
+template <typename T, std::convertible_to<T> E>
+std::vector<T> operator*(E constant, std::vector<T> vec) {
+  int N{vec.size()};
+  std::vector<T> result(N);
+
+  int size{sizeof(T) * N};
+
+  // Allocate on device
+  T *d_res;
+  cudaMalloc(&d_res, size);
+
+  // Launch kernel
+  cudaMemcpy(d_res, vec.data(), size, cudaMemcpyHostToDevice);
+  vec_multiply<<<N/256, 256>>>(d_res, constant, N);
+  cudaMemcpy(result.data(), d_res, size, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_res);
+
+  return result;
+}
+
+// Overload division of vector with constant
+template <typename T, std::convertible_to<T> E>
+std::vector<T> operator/(E constant, std::vector<T> vec) {
+  int N{vec.size()};
+  std::vector<T> result(N);
+
+  int size{sizeof(T) * N};
+
+  // Allocate on device
+  T *d_res;
+  cudaMalloc(&d_res, size);
+
+  // Launch kernel
+  cudaMemcpy(d_res, vec.data(), size, cudaMemcpyHostToDevice);
+  vec_divide<<<N/256, 256>>>(d_res, constant, N);
+  cudaMemcpy(result.data(), d_res, size, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_res);
+
+  return result;
+}
+
+// Overload of operator+= with matrices
+template <typename T>
+void operator+=(std::vector<T>& vec, const Matrix<T>& m) {
+  int N{vec.size()};
+  std::vector<T> result(N);
+
+  int size{sizeof(T) * N};
+
+  // Allocate on device
+  T *d_vec, *d_mat;
+  cudaMalloc(&d_vec, size);
+  cudaMalloc(&d_mat, size);
+
+  // Launch kernel
+  cudaMemcpy(d_vec, vec.data(), size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_mat, (m.data()).data(), size, cudaMemcpyHostToDevice);
+  vec_add<<<N/256, 256>>>(d_vec, d_res, N);
+  cudaMemcpy(vec.data(), d_vec, size, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_vec);
+  cudaFree(d_mat);
+}
+
+// Overload of operator-= with matrices
+template <typename T>
+void operator-=(std::vector<T>& vec, const Matrix<T>& m) {
+  int N{vec.size()};
+  std::vector<T> result(N);
+
+  int size{sizeof(T) * N};
+
+  // Allocate on device
+  T *d_vec, *d_mat;
+  cudaMalloc(&d_vec, size);
+  cudaMalloc(&d_mat, size);
+
+  // Launch kernel
+  cudaMemcpy(d_vec, vec.data(), size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_mat, (m.data()).data(), size, cudaMemcpyHostToDevice);
+  vec_sub<<<N/256, 256>>>(d_vec, d_res, N);
+  cudaMemcpy(vec.data(), d_vec, size, cudaMemcpyDeviceToHost);
+
+  cudaFree(d_vec);
+  cudaFree(d_mat);
+}
+
+// Ostream operator for vectors
+template <typename T>
+std::ostream& operator<<(std::ostream& out, const std::vector<T>& vec) {
+  out << *vec.begin();
+  std::for_each(vec.begin()+1, vec.end(), [&out](T x) {
+		out << ',';
+		out << x;
+	  }); 
+
+  return out;
+}
+
+#endif
