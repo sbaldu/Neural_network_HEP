d56aa424c98994a786a204f4eb47095d8ef45e3d
sbaldu <simone.balducci00@gmail.com>
2023-07-18 11:49:57 +0200

    Fix bugs in cuda overloads of vector operations

--- src/cuda/DataFormats/VectorOperations.h
+++ src/cuda/DataFormats/VectorOperations.h
@@ -15,10 +15,10 @@
 // Overload sum operator for vectors
 template <typename T>
 std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2) {
-  int N{v1.size()};
+  const size_t N{v1.size()};
   std::vector<T> result(N);
 
-  int size{sizeof(T) * N};
+  const size_t size{sizeof(T) * N};
 
   // Allocate on device
   T *d_v1, *d_v2, *d_res;
@@ -26,10 +26,14 @@ std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2) {
   cudaMalloc(&d_v2, size);
   cudaMalloc(&d_res, size);
 
+  // Create working division
+  const int block_size{32};
+  const int grid_size{(int)std::ceil(N / (float)(block_size))};
+
   // Launch kernel
   cudaMemcpy(d_v1, v1.data(), size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_v2, v2.data(), size, cudaMemcpyHostToDevice);
-  vec_add<<<N / 256, 256>>>(d_v1, d_v2, d_res, N);
+  vec_add<<<grid_size, block_size>>>(d_v1, d_v2, d_res, N);
   cudaMemcpy(result.data(), d_res, size, cudaMemcpyDeviceToHost);
 
   cudaFree(d_v1);
@@ -42,10 +46,10 @@ std::vector<T> operator+(const std::vector<T>& v1, const std::vector<T>& v2) {
 // Overload subtraction
 template <typename T>
 std::vector<T> operator-(const std::vector<T>& v1, const std::vector<T>& v2) {
-  int N{v1.size()};
+  const size_t N{v1.size()};
   std::vector<T> result(N);
 
-  int size{sizeof(T) * N};
+  const size_t size{sizeof(T) * N};
 
   // Allocate on device
   T *d_v1, *d_v2, *d_res;
@@ -53,10 +57,14 @@ std::vector<T> operator-(const std::vector<T>& v1, const std::vector<T>& v2) {
   cudaMalloc(&d_v2, size);
   cudaMalloc(&d_res, size);
 
+  // Create working division
+  const int block_size{32};
+  const int grid_size{(int)std::ceil(N / (float)(block_size))};
+
   // Launch kernel
   cudaMemcpy(d_v1, v1.data(), size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_v2, v2.data(), size, cudaMemcpyHostToDevice);
-  vec_sub<<<N / 256, 256>>>(d_v1, d_v2, d_res, N);
+  vec_sub<<<grid_size, block_size>>>(d_v1, d_v2, d_res, N);
   cudaMemcpy(result.data(), d_res, size, cudaMemcpyDeviceToHost);
 
   cudaFree(d_v1);
@@ -69,18 +77,22 @@ std::vector<T> operator-(const std::vector<T>& v1, const std::vector<T>& v2) {
 // Overload product of vector with constant
 template <typename T, std::convertible_to<T> E>
 std::vector<T> operator*(E constant, std::vector<T> vec) {
-  int N{vec.size()};
+  const size_t N{vec.size()};
   std::vector<T> result(N);
 
-  int size{sizeof(T) * N};
+  const size_t size{sizeof(T) * N};
 
   // Allocate on device
   T* d_res;
   cudaMalloc(&d_res, size);
 
+  // Create working division
+  const int block_size{32};
+  const int grid_size{(int)std::ceil(N / (float)(block_size))};
+
   // Launch kernel
   cudaMemcpy(d_res, vec.data(), size, cudaMemcpyHostToDevice);
-  vec_multiply<<<N / 256, 256>>>(d_res, constant, N);
+  vec_multiply<<<grid_size, block_size>>>(d_res, constant, N);
   cudaMemcpy(result.data(), d_res, size, cudaMemcpyDeviceToHost);
 
   cudaFree(d_res);
@@ -91,18 +103,22 @@ std::vector<T> operator*(E constant, std::vector<T> vec) {
 // Overload division of vector with constant
 template <typename T, std::convertible_to<T> E>
 std::vector<T> operator/(E constant, std::vector<T> vec) {
-  int N{vec.size()};
+  const size_t N{vec.size()};
   std::vector<T> result(N);
 
-  int size{sizeof(T) * N};
+  const size_t size{sizeof(T) * N};
 
   // Allocate on device
   T* d_res;
   cudaMalloc(&d_res, size);
 
+  // Create working division
+  const int block_size{32};
+  const int grid_size{(int)std::ceil(N / (float)(block_size))};
+
   // Launch kernel
   cudaMemcpy(d_res, vec.data(), size, cudaMemcpyHostToDevice);
-  vec_divide<<<N / 256, 256>>>(d_res, constant, N);
+  vec_divide<<<grid_size, block_size>>>(d_res, constant, N);
   cudaMemcpy(result.data(), d_res, size, cudaMemcpyDeviceToHost);
 
   cudaFree(d_res);
@@ -113,20 +129,24 @@ std::vector<T> operator/(E constant, std::vector<T> vec) {
 // Overload of operator+= with matrices
 template <typename T>
 void operator+=(std::vector<T>& vec, const Matrix<T>& m) {
-  int N{vec.size()};
+  const size_t N{vec.size()};
   std::vector<T> result(N);
 
-  int size{sizeof(T) * N};
+  const size_t size{sizeof(T) * N};
 
   // Allocate on device
   T *d_vec, *d_mat;
   cudaMalloc(&d_vec, size);
   cudaMalloc(&d_mat, size);
 
+  // Create working division
+  const int block_size{32};
+  const int grid_size{(int)std::ceil(N / (float)(block_size))};
+
   // Launch kernel
   cudaMemcpy(d_vec, vec.data(), size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_mat, (m.data()).data(), size, cudaMemcpyHostToDevice);
-  vec_add<<<N / 256, 256>>>(d_vec, d_res, N);
+  vec_add<<<grid_size, block_size>>>(d_vec, d_mat, N);
   cudaMemcpy(vec.data(), d_vec, size, cudaMemcpyDeviceToHost);
 
   cudaFree(d_vec);
@@ -136,20 +156,24 @@ void operator+=(std::vector<T>& vec, const Matrix<T>& m) {
 // Overload of operator-= with matrices
 template <typename T>
 void operator-=(std::vector<T>& vec, const Matrix<T>& m) {
-  int N{vec.size()};
+  const size_t N{vec.size()};
   std::vector<T> result(N);
 
-  int size{sizeof(T) * N};
+  const size_t size{sizeof(T) * N};
 
   // Allocate on device
   T *d_vec, *d_mat;
   cudaMalloc(&d_vec, size);
   cudaMalloc(&d_mat, size);
 
+  // Create working division
+  const int block_size{32};
+  const int grid_size{(int)std::ceil(N / (float)(block_size))};
+
   // Launch kernel
   cudaMemcpy(d_vec, vec.data(), size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_mat, (m.data()).data(), size, cudaMemcpyHostToDevice);
-  vec_sub<<<N / 256, 256>>>(d_vec, d_res, N);
+  vec_sub<<<grid_size, block_size>>>(d_vec, d_mat, N);
   cudaMemcpy(vec.data(), d_vec, size, cudaMemcpyDeviceToHost);
 
   cudaFree(d_vec);
