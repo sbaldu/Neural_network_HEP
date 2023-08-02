f897223aa8178e7c3a73dd2ac7cfba07f769e8ea
sbaldu <simone.balducci00@gmail.com>
2023-08-02 12:41:03 +0200

    Update preprocessing of the full dataset and study of PCA. Split with 80/10/10 rule

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -5,63 +5,74 @@ from sklearn.decomposition import PCA
 from psynlig import pca_residual_variance
 import pandas as pd
 
-data = pd.read_csv("../data/training.csv")
-print(data.head())
+# Load the full dataset
+full_dataset = pd.read_csv('../data/higgs_ml_full_data.csv')
 
 # First of all, we want to convert the labels into integers
 # We indicate signal events with 1 and background events with 0
-data['Label'].replace(inplace=True, to_replace='s', value=1)
-data['Label'].replace(inplace=True, to_replace='b', value=0)
+full_dataset['Label'].replace(inplace=True, to_replace='s', value=1)
+full_dataset['Label'].replace(inplace=True, to_replace='b', value=0)
+
+# Remove the 'Kaggle' features, which are of no use
+full_dataset.drop(labels=['KaggleSet', 'KaggleWeight'], axis=1, inplace=True)
+# Also remove the Event_id and weight columns
+full_dataset.drop(labels=['EventId', 'Weight'], axis=1, inplace=True)
 
 # Next, we shift all the columns one position to the left, so that the labels are
 # in the first column
-cols = data.columns.tolist()
+cols = full_dataset.columns.tolist()
 cols = cols[-1:] + cols[:-1]
-data = data[cols]
+full_dataset = full_dataset[cols]
 
 # We want to remove the events where one or more features have the value '-999', because it means that
 # the feature couldn't be computed
 to_be_discarded = []
-for i, row in data.iterrows():
+for i, row in full_dataset.iterrows():
     if -999.0 in row.values.tolist():
         to_be_discarded.append(i)
-data = data.drop(to_be_discarded)
-print(data)
+full_dataset = full_dataset.drop(to_be_discarded)
+print(full_dataset)
+
+full_dataset_len = len(full_dataset.index)
+training_dataset = full_dataset.iloc[:int(0.8 * full_dataset_len), :]
+validation_dataset = full_dataset.iloc[int(0.8 * full_dataset_len) : int(0.9 * full_dataset_len), :]
+test_dataset = full_dataset.iloc[int(0.9 * full_dataset_len) : , :]
 
 # Standardize all the feature in the dataset
-for col in data:
+for col in training_dataset:
     if col[0:3] in ['DER', 'PRI']:
         scaler = StandardScaler()
-        data[col] = scaler.fit_transform(data[col].to_numpy().reshape(-1, 1))
+        training_dataset[col] = scaler.fit_transform(training_dataset[col].to_numpy().reshape(-1, 1))
 
-# Remove the Event_id and weight columns since it is of no use
-data.drop(labels=['EventId', 'Weight'], axis=1, inplace=True)
+        # We also normalize the validation and test datasets, with the same mean and 
+        # standard variance of the training dataset
+        validation_dataset[col] = scaler.transform(validation_dataset[col].to_numpy().reshape(-1, 1))
+        test_dataset[col] = scaler.transform(test_dataset[col].to_numpy().reshape(-1, 1))
+print(training_dataset)
 
-# Perform principal component analysis and plot the residual variance in function
-# of the number of components
+# Perform principal component analysis on the training dataset and plot the residual 
+# variance in function of the number of components
+fig, ax = plt.subplots(1, 2, sharey=None)
 pca_r = PCA()
-pca_r.fit_transform(data)
+pca_r.fit_transform(training_dataset.iloc[:,1:])
 
-pca_residual_variance(pca_r, marker='o')
-plt.grid(linewidth=0.2)
+
+pca_residual_variance(pca_r, marker='o', axi=ax[0])
+ax[0].grid(linewidth=0.2)
+
+# After doing the pca, we try to plot the two categories in function of the first two 
+# principal components
+pca = PCA(n_components=2)
+training_reduced = pca_r.fit_transform(training_dataset.iloc[:,1:])
+scatter = ax[1].scatter(training_reduced[:,0], training_reduced[:,1], c=training_dataset['Label'])
+ax[1].grid(linewidth=0.2)
+ax[1].legend(handles=scatter.legend_elements()[0], labels=['Signal', 'Background'])
+ax[1].set_xlabel("First principal component")
+ax[1].set_ylabel("Second principal component")
 figure = plt.gcf()
-figure.set_size_inches(8, 5)
-plt.savefig('../tex/img/residual_variance.png', dpi = 100)
+figure.set_size_inches(11, 5)
+plt.savefig('../tex/img/pca.png', dpi = 100)
 
-# components = []
-# variance_ratios = []
-# for i in range(1, 31):
-#     pca_i = PCA(n_components=i)
-#     pca_i.fit_transform(data)
-#     components.append(i)
-#     variance_ratios.append(np.sum(pca_i.explained_variance_ratio_))
-# plt.plot(components, variance_ratios, marker='o')
-# plt.xlabel('Number of components')
-# plt.ylabel('Explained variance ratio')
-# plt.grid(linewidth=0.2)
-# plt.show()
-
-# data.to_csv("../data/training_filtered_processed.csv", index=None)
-# reduced_data.to_csv("../data/training_filtered_processed_reduced.csv", index=None)
-# data.to_csv("../data/training_processed.csv", index=None)
-# reduced_data.to_csv("../data/training_processed_reduced.csv", index=None)
+training_dataset.to_csv('new_training_data.csv', index=None)
+validation_dataset.to_csv('new_validation_data.csv', index=None)
+test_dataset.to_csv('new_test_data.csv', index=None)
