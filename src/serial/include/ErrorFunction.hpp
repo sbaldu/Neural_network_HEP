25f4270fa673373be150db7ecf069c821ef34e23
sbaldu <simone.balducci00@gmail.com>
2023-11-11 14:45:38 +0100

    Document loss functions

--- src/serial/include/ErrorFunction.hpp
+++ src/serial/include/ErrorFunction.hpp
@@ -1,3 +1,11 @@
+/// @file ErrorFunction.hpp
+/// @brief Error functions for neural networks
+///
+/// @details Error functions are used to determine the error of a neural network
+/// by comparing the output of the network to the expected output. The error
+/// function is used to determine the gradient of the error with respect to the
+/// weights and biases of the network. The gradient is then used to update the
+/// weights and biases of the network using gradient descent.
 
 #ifndef ErrorFunction_h
 #define ErrorFunction_h
@@ -12,8 +20,20 @@
 template <typename T>
 using shared = std::shared_ptr<T>;
 
+/// @brief Mean squared error function
+/// @tparam T The type of the node values
+/// @tparam W The type of the weights
+/// @tparam Activator The activation function used by the network
 template <typename T, typename W, template <typename E> typename Activator>
 struct MeanSquaredError {
+  /// @brief Calculate the error of the network
+  /// @tparam U The type of the expected values
+  /// @param node_values The values of the nodes in the network
+  /// @param expected_values The expected values of the nodes in the network
+  /// @return The error of the network
+  ///
+  /// @details The error is calculated by taking the mean of the squared
+  /// difference between the node values and the expected values.
   template <typename U>
   double operator()(const std::vector<T>& node_values, const std::vector<U>& expected_values) {
     double error{};
@@ -26,7 +46,15 @@ struct MeanSquaredError {
     return error;
   }
 
-  // Derivative of the error function with respect to the activated node values
+  /// @brief Calculate the gradient of the error with respect to the weights and
+  /// biases of the network
+  /// @tparam U The type of the expected values
+  /// @param expected_values The expected values of the nodes in the network
+  /// @param layer_id The id of the layer to calculate the gradient for
+  /// @param layers The layers of the network
+  /// @param weights The weights of the network
+  /// @return The gradient of the error with respect to the weights and biases
+  /// of the network
   template <typename U>
   std::vector<double> grad(const std::vector<U>& expected_values,
                            int layer_id,
@@ -56,6 +84,7 @@ struct MeanSquaredError {
   }
 };
 
+// TODO: Implement other error functions
 /* struct MeanAbsoluteError { */
 /*   double operator()() {} */
 /*   double grad() {} */
