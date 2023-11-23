250caf045017c97705aef11bf499ea483abd5c83
sbaldu <simone.balducci00@gmail.com>
2023-11-23 19:48:22 +0100

    Add main header file

---
+++ src/nnhep.hpp
@@ -0,0 +1,12 @@
+
+#ifndef nnhep_hpp
+#define nnhep_hpp
+
+#include "nnhep/DataFormats/Matrix.hpp"
+#include "nnhep/DataFormats/VectorOperations.hpp"
+#include "nnhep/headers/Activators.hpp"
+#include "nnhep/headers/ErrorFunction.hpp"
+#include "nnhep/headers/Layer.hpp"
+#include "nnhep/headers/Network.hpp"
+
+#endif
