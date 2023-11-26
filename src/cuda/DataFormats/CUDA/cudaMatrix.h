015ae5d250ad39d71cceec513aba47e97d77d74a
sbaldu <simone.balducci00@gmail.com>
2023-11-26 11:21:01 +0100

    Rewrite constructor and destructor of device matrix

--- src/cuda/DataFormats/CUDA/cudaMatrix.h
+++ src/cuda/DataFormats/CUDA/cudaMatrix.h
@@ -4,13 +4,25 @@
 
 #pragma once
 
+#include <iostream>
+
 template <typename T>
 struct matrix_t {
   T* data;
   int rows;
   int cols;
 
-  matrix_t(int n_rows, int n_cols, T* data) : data{data}, rows{n_rows}, cols{n_cols} {}
+  matrix_t(int n_rows, int n_cols) : rows{n_rows}, cols{n_cols} {
+	cudaMalloc(&data, rows * cols * sizeof(T));
+  }
+  /* matrix_t(int n_rows, int n_cols, T* data) : data{data}, rows{n_rows}, cols{n_cols} { */
+	/* cudaMalloc(&data, rows * cols * sizeof(T)); */
+  /* } */
+
+  ~matrix_t() { 
+	std::cout << "The destructor of cuda matrix is called" << std::endl;
+	cudaFree(data);
+  }
 
   __host__ __device__ T& operator[](int index) { return data[index]; }
   __host__ __device__ const T& operator[](int index) const { return data[index]; }
