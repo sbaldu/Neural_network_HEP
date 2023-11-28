37f27c1bccde975b04adb75ef6b853bb4e496ad7
sbaldu <simone.balducci00@gmail.com>
2023-11-28 21:10:35 +0100

    Rename members of cudaMatrix

--- src/cuda/DataFormats/CUDA/cudaMatrix.cuh
+++ src/cuda/DataFormats/CUDA/cudaMatrix.cuh
@@ -11,31 +11,31 @@ using Bytes = std::size_t;
 template <typename T>
 struct cudaMatrix {
 private:
-  int rows;
-  int cols;
-  T* data;
+  int m_nrows;
+  int m_ncols;
+  T* m_data;
 
 public:
-  cudaMatrix(int n_rows, int n_cols) : rows{n_rows}, cols{n_cols} {
-    const Bytes bytes{rows * cols * sizeof(T)};
-    cudaMalloc(&data, bytes);
+  cudaMatrix(int n_rows, int n_cols) : m_nrows{n_rows}, m_ncols{n_cols} {
+    const Bytes bytes{m_nrows * m_ncols * sizeof(T)};
+    cudaMalloc(&m_data, bytes);
   }
-  cudaMatrix(int n_rows, int n_cols, T* data) : data{data}, rows{n_rows}, cols{n_cols} {
-    const Bytes bytes{rows * cols * sizeof(T)};
-    cudaMalloc(&data, bytes);
-    cudaMemcpy(&(this->data), data, bytes, cudaMemcpyHostToDevice);
+  cudaMatrix(int n_rows, int n_cols, T* data) : m_data{data}, m_nrows{n_rows}, m_ncols{n_cols} {
+    const Bytes bytes{m_nrows * m_ncols * sizeof(T)};
+    cudaMalloc(&m_data, bytes);
+    cudaMemcpy(&(this->m_data), data, bytes, cudaMemcpyHostToDevice);
   }
 
-  ~cudaMatrix() { cudaFree(data); }
+  ~cudaMatrix() { cudaFree(m_data); }
 
-  T* data() { return data; }
-  const T* data() const { return data; }
+  T* data() { return m_data; }
+  const T* data() const { return m_data; }
 
-  __device__ int rows() const { return rows; }
-  __device__ int cols() const { return cols; }
+  __device__ int rows() const { return m_nrows; }
+  __device__ int cols() const { return m_ncols; }
 
-  __device__ T& operator[](int index) { return data[index]; }
-  __device__ const T& operator[](int index) const { return data[index]; }
+  __device__ T& operator[](int index) { return m_data[index]; }
+  __device__ const T& operator[](int index) const { return m_data[index]; }
 };
 
 #endif
