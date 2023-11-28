27391d08d3b84ccce1f4985728c72a1d84fc6a50
sbaldu <simone.balducci00@gmail.com>
2023-11-28 12:06:30 +0100

    Rename `matrix_t` as `cudaMatrix`

--- src/cuda/DataFormats/CUDA/cudaMatrix.cuh
+++ src/cuda/DataFormats/CUDA/cudaMatrix.cuh
@@ -9,22 +9,22 @@
 using Bytes = std::size_t;
 
 template <typename T>
-struct matrix_t {
+struct cudaMatrix {
   int rows;
   int cols;
   T* data;
 
-  matrix_t(int n_rows, int n_cols) : rows{n_rows}, cols{n_cols} {
+  cudaMatrix(int n_rows, int n_cols) : rows{n_rows}, cols{n_cols} {
     const Bytes bytes{rows * cols * sizeof(T)};
     cudaMalloc(&data, bytes);
   }
-  matrix_t(int n_rows, int n_cols, T* data) : data{data}, rows{n_rows}, cols{n_cols} {
+  cudaMatrix(int n_rows, int n_cols, T* data) : data{data}, rows{n_rows}, cols{n_cols} {
     const Bytes bytes{rows * cols * sizeof(T)};
     cudaMalloc(&data, bytes);
     cudaMemcpy(&(this->data), data, bytes, cudaMemcpyHostToDevice);
   }
 
-  ~matrix_t() { cudaFree(data); }
+  ~cudaMatrix() { cudaFree(data); }
 
   __device__ T& operator[](int index) { return data[index]; }
   __device__ const T& operator[](int index) const { return data[index]; }
