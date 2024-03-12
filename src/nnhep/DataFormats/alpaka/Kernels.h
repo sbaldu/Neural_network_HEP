498032ae10d64e3b5c7605b84e4223dd0300ddbf
Simone Balducci <simone.balducci00@gmail.com>
2024-03-12 21:31:43 +0100

    Add matrix multiplication kernel

--- src/nnhep/DataFormats/alpaka/Kernels.h
+++ src/nnhep/DataFormats/alpaka/Kernels.h
@@ -41,8 +41,10 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
   template <typename T>
   struct KernelDecrement {
     template <typename TAcc>
-    ALPAKA_FN_ACC void operator()(
-        const TAcc& acc, const T* a, T* b, size_t n_points) const {
+    ALPAKA_FN_ACC void operator()(const TAcc& acc,
+                                  const T* a,
+                                  T* b,
+                                  size_t n_points) const {
       for (auto i : cms::alpakatools::elements_with_stride(acc, n_points)) {
         b[i] -= a[i];
       }
@@ -104,6 +106,43 @@ namespace ALPAKA_ACCELERATOR_NAMESPACE {
         const TAcc& acc, const T* a, const T* b, T* c, size_t n_points) const {
       for (auto i : cms::alpakatools::elements_with_stride(acc, n_points)) {
         c[i] = a[i] * b[i];
+      }
+    }
+  };
+
+  // C = A * B
+  template <typename T>
+  struct KernelMatrixMultiplication {
+    template <typename TAcc>
+    ALPAKA_FN_ACC void operator()(const TAcc& acc,
+                                  const T* a,
+                                  const T* b,
+                                  T* c,
+                                  size_t a_rows,
+                                  size_t b_cols,
+                                  size_t n) const {
+      for (auto nd_index :
+           cms::alpakatools::elements_with_stride_nd(acc, Vec2D{a_rows, b_cols})) {
+        for (size_t k = 0; k < n; ++k) {
+          c[nd_index[0] * b_cols + nd_index[1]] +=
+              a[nd_index[0] * n + k] * b[k * b_cols + nd_index[1]];
+        }
+      }
+    }
+  };
+
+  // A = A * B
+  template <typename T>
+  struct KernelMatrixMultiplication_inplace {
+    template <typename TAcc>
+    ALPAKA_FN_ACC void operator()(
+        const TAcc& acc, T* a, const T* b, size_t a_rows, size_t b_cols, size_t n) const {
+      for (auto nd_index :
+           cms::alpakatools::elements_with_stride_nd(acc, Vec2D{a_rows, b_cols})) {
+        for (size_t k = 0; k < n; ++k) {
+          a[nd_index[0] * b_cols + nd_index[1]] +=
+              a[nd_index[0] * n + k] * b[k * b_cols + nd_index[1]];
+        }
       }
     }
   };
